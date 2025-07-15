e2ceff6ecad5b5a190630a470f1605e8825badd7
Rifat Hossain <hossainrifat221@gmail.com>
2025-07-16 01:38:30 +0600

    Update Median of Two Sorted Arrays.java

--- Median of Two Sorted Arrays/Median of Two Sorted Arrays.java
+++ Median of Two Sorted Arrays/Median of Two Sorted Arrays.java
@@ -1,26 +1,14 @@
 class Solution {
-public:
-    double findMedianSortedArrays(vector<int>& v1, vector<int>& v2) {
-
-       vector<int> v(v1.size() + v2.size());
-    // Merging the two vectors
-    merge(v1.begin(), v1.end(), v2.begin(),
-          v2.end(), v.begin());
-          sort(v.begin(),v.end());
-
-        int sz=v.size();
-        double ans;
-        if(sz&1)
-        {
-            int cal=((sz+1)/2)-1;
-            ans=v[cal];
-        }
-        else
-        {
-            int cal=(sz)/2;
-            int cal2=cal-1;
-            ans=(double)(v[cal]+v[cal2])/2;
-        }
-        return ans;
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int[] v = new int[nums1.length + nums2.length];
+    System.arraycopy(nums1, 0, v, 0, nums1.length);
+    System.arraycopy(nums2, 0, v, nums1.length, nums2.length);
+    Arrays.sort(v);
+    int sz = v.length;
+    if (sz % 2 == 1) {
+        return v[sz / 2];
+    } else {
+        return (v[sz / 2] + v[(sz / 2) - 1]) / 2.0;
     }
-};
+    }
+}
